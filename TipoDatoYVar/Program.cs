03cf31926332dc9d2830314b305e43dff431af29
TomasCienega <tomascienega0206@gmail.com>
2025-11-17 17:46:09 -0600

    Practica de variables y como declarar

---
+++ TipoDatoYVar/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+//En C#, los números enteros son un tipo de valor que representa números sin decimales. Los números enteros en C# incluyen los siguientes tipos: byte, short, int, long.
+
+//byte es un número entero sin signo que puede almacenar valores entre 0 y 255.
+//short es un número entero con signo que puede almacenar valores entre -32768 y 32767.
+//int es un número entero con signo que puede almacenar valores entre -2147483648 y 2147483647.
+//long es un número entero con signo que puede almacenar valores entre -9223372036854775808 y 9223372036854775807.
+
+byte edad = 30;
+short añosDeExperiencia = 5;
+int númeroDeEmpleados = 1000;
+long poblaciónMundial = 7_000_000_000;
+
+//En C#, los números flotantes son un tipo de valor que representan números con decimales. Los números flotantes en C# incluyen los siguientes tipos: float y double.
+
+//float es un número de punto flotante con precisión simple que puede almacenar valores con una precisión de 7 dígitos decimales aproximadamente.
+//double es un número de punto flotante con precisión doble que puede almacenar valores con una precisión de 15-16 dígitos decimales aproximadamente.
+
+float pi = 3.1415926535f;
+double tasaDeInteres = 0.05;
+
+//Decimal en C#
+//En C#, los decimales son un tipo de valor que representan números con una precisión exacta y se utilizan para representar valores monetarios y otros valores que requieren una precisión exacta.
+
+//El tipo decimal en C# es un número de punto flotante con precisión exacta y puede almacenar valores con una precisión de hasta 28-29 dígitos decimales.
+
+decimal salario = 75000.00m;
+
+//En C#, los caracteres son un tipo de valor que representa un solo carácter en el alfabeto Unicode. 
+//    El tipo char en C# se utiliza para representar un carácter y se puede almacenar en una sola variable. 
+//    Los caracteres se representan entre comillas simples, por ejemplo: 'a', 'b', 'c', etc.
+
+char inicial = 'J';
+
+//En C#, los tipos booleanos son un tipo de valor que solo pueden tener dos valores: true o false. 
+//    El tipo bool en C# se utiliza para representar valores lógicos o para tomar decisiones en el código.
+
+bool esMayorDeEdad = true;
+
+string nombre = "Tomas";
+
