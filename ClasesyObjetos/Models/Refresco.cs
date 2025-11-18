c4b9340667aaa9ba0ef2243fa9604caa75177641
TomasCienega <tomascienega0206@gmail.com>
2025-11-18 16:03:02 -0600

    Segundo Video Clases y Objetos

---
+++ ClasesyObjetos/Models/Refresco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesyObjetos.Models
+{
+    internal class Refresco : Bebida
+    {
+        public Refresco(string Nombre, int Cantidad) : base(Nombre, Cantidad)
+        {
+        }
+    }
+}
