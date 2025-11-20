08ff2d8cc7ed279b37ff2cf044c8ec696fed2ef9
TomasCienega <tomascienega0206@gmail.com>
2025-11-20 12:47:40 -0600

    Consultas Sql

---
+++ SqlConsultas/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClasesyObjetos.Models;
+using Microsoft.Data.SqlClient;
+
+
+//Una clase sirve para crear un objeto de la vida real, la clase define atributos y acciones.
+
+Bebida bebida = new Bebida("Sangría",1000);
+bebida.Beber(800);
+
+if(bebida.Cantidad < 300)
+{
+    Console.WriteLine("Bebe más despacio o comprate otra ya solo quedan " + bebida.Cantidad + "ml");
+}
+else
+{
+    Console.WriteLine("Aún queda bastante bebida disfruta tus " + bebida.Cantidad + "ml");
+}
+
+Refresco sangria = new Refresco("Sangría", 700);
+sangria.Beber(20);
+Console.WriteLine("La bebida más deliciosa, te quedan " + sangria.Cantidad + "ml");
+
+//Clase 4 INTERFACES
+
+var bebidaGasificada = new Jugo("Uva", 190, 20);
+MostrarRecomendacion(bebidaGasificada);
+Console.WriteLine("Tu bebida tiene: " + bebidaGasificada.Gas +" por ciento de Gas");
+
+var bebidaGasificada2 = new Refresco("Sangría", 500);
+MostrarRecomendacion(bebidaGasificada2);
+
+
+static void MostrarRecomendacion(IBebidaGasificada bebida)
+{
+    bebida.BotellasRecomendadas();
+}
+
+Console.WriteLine("\n");
+
+//Clase 5 CONEXIOÓN A BD
+
+RefrescoBD conn = new RefrescoBD();
+
+SqlConnection con = conn.ObtenerConexion();
+
+//Try para probar la conexión a la BD
+try
+{
+    con.Open();
+    Console.WriteLine("Conexión exitosa!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error: " + ex.Message);
+}
+finally
+{
+    con.Close(); // <- cierre manual
+}
+Console.WriteLine("\n");
+
+//Obtención de los datos
+RefrescoBD refrescoBD = new RefrescoBD();
+var refrescos = refrescoBD.Get();
+foreach (var refre in refrescos)
+{ 
+    Console.WriteLine(refre.Nombre);
+}
+
+
+Console.WriteLine("\n");
+//Clase 6 CONSULTAS SQL
+
+//Inserción de datos
+
+//var refresco = new Refresco("Caballito", 400);
+//refresco.Marca = "PepsiCo";
+//refresco.Gas = 100;
+//refrescoBD.Add(refresco);
+
+
+//Edición de 1 dato
+
+//var refresco = new Refresco("Caballito", 500);
+//refresco.Marca = "PepsiCO";
+//refresco.Gas = 60;
+//refrescoBD.Edit(refresco,6);
+
+//Eliminación de 1 dato
+
+refrescoBD.Eliminar(6);
