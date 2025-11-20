be9617a3a221a05c7558545dbf6dc80abc970729
TomasCienega <tomascienega0206@gmail.com>
2025-11-20 13:42:46 -0600

    Serializacion y DesSerializacion de objetos
    JSON

---
+++ SerializacionDeObjetos/Models/Refresco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesyObjetos.Models
+{
+    internal class Refresco : Bebida,IBebidaGasificada
+    {
+        public Refresco(string Nombre, int Cantidad) : base(Nombre, Cantidad)
+        {
+        }
+
+        public int Gas { get; set; }
+        public string Marca { get; set; }
+
+        public void BotellasRecomendadas()
+        {
+            Console.WriteLine("Las recomendadas son 2 botellas por día");
+        }
+    }
+}
