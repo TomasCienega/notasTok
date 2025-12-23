e7a23942783ab5d3beaf1a500ab944c3aed9dc10
Tomas Cienega <tomascienega0206@gmail.com>
2025-12-23 17:52:22 -0600

    revision

--- RazorPagesMovie/RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184f9b6f27358d8cd6435eff2c3686232af69bfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00399625b129112bf510a364ffd573f326e47226")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
